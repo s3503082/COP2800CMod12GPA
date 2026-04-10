23489554b57861d67d835136fd8d6e869ef5829c
s3503082 <s3503082@students.fscj.edu>
2026-04-09 22:57:44 -0400

    Add files via upload

---
+++ edu/fscj/cop2800c/util/DataWrapper.java
@@ -0,0 +1,30 @@
+// DataWrapper.java
+// JaTavion Washington
+// 04/09/2026
+// Handling code to incorporate a generic class
+
+package edu.fscj.cop2800c.util;
+
+import java.util.List;
+
+public class DataWrapper<T> {
+    private T value;
+
+    public DataWrapper() {
+        value = null;
+    }
+
+    public DataWrapper(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public static <T> void displayList(List<DataWrapper<T>> list) {
+        for (DataWrapper<T> element : list) {
+            System.out.println(element.getValue());
+        }
+    }
+}
